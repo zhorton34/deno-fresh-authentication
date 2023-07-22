a47f9c77ecc6be7c933d7f9c5d90bca80ec95fae
Zak Horton <zhorton999@gmail.com>
2023-07-22 07:04:42 -0700

    Update

--- .history/routes/index_20230722055538.tsx
+++ .history/routes/index_20230722055538.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Find How App</title>
+        <title>fresh-authentication</title>
       </Head>
       <div class="px-4 py-8 mx-auto bg-[#86efac]">
         <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
@@ -18,8 +18,9 @@ export default function Home() {
             height="128"
             alt="the fresh logo: a sliced lemon dripping with juice"
           />
-          <h1 class="text-4xl font-bold">Welcome to Find How</h1>
+          <h1 class="text-4xl font-bold">Welcome to fresh</h1>
           <p class="my-4">
+            Try updating this message in the
             <code class="mx-2">./routes/index.tsx</code> file, and refresh.
           </p>
           <Counter count={count} />
